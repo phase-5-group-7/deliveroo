45ae5f1cda31d3d8b093e8e94c7b729c7501a034
Student <venus.kabera@student.moringaschool.com>
2023-04-14 09:56:07 +0300

    added routes

--- backend/app/controllers/orders_controller.rb
+++ backend/app/controllers/orders_controller.rb
@@ -1,49 +1,42 @@
 class OrdersController < ApplicationController
-    before_action :set_order, only [:show, :edit ,:update, :destroy]
-  def index
-    @orders = Order.all
-  end
-
-  def show
-  end
-
-  def new
-    @order = Order.new
-  end
-
-  def create
-    @order = current_user.orders.build(order_params)
-
-    if @order.save
-      redirect_to @order, notice: 'order was successfully created.'
-    else
-      render :new
+    before_action :set_order, only: [:show, :edit, :update, :destroy]
+  
+    def index
+      @orders = Order.all
+      render json: @order
+    end
+  
+    def create
+      @order = Order.new(order_params)
+  
+      if @order.save
+        app_response(status: :created, data: @order)
+      else
+        app_response(status: :unprocessable_entity, data: @order.errors, message: 'failed to create order')
+      end
+    end
+  
+    def update
+      if @order.update(order_params)
+        app_response(data: { info: 'updated order successfully' })
+      else
+        app_response(status: :unprocessable_entity, data: { info: 'something went wrong, could not update order' }, message: 'failed to update order')
+      end
+    end
+  
+    def destroy
+      @order.destroy
+      app_response(status: :no_content, message: 'success', data: { info: 'deleted order successfully' })
+    end
+  
+    private
+  
+    def set_order
+      @order = Order.find(params[:id])
+    end
+  
+    def order_params
+      params.require(:order).permit(:username, :phone_number, :recipient_name, :recipient_phone_number, :description, :weight, :drop_off, :pick_up, :distance, :price, :status)
     end
   end
-
-  def edit
-  end
-
-  def update
-    if @order.update(order_params)
-    else
-      render :edit
-    end
-  end
-
-  def destroy
-    @order.destroy
-
-  end
-
-  private
-
-  def set_order
-    @order = order.find(params[:id])
-  end
-
-  def order_params
-    params.require(:order).permit(:username, :phone_number, :recepient_name, :recepient_phone_number, :description, :weight, :drop_off, :pick_up, :distance, :price, :status)
-  end
-end
-
+  
--- backend/app/models/order.rb
+++ backend/app/models/order.rb
@@ -4,4 +4,3 @@ class Order < ApplicationRecord
   validates :username, :phone_number, :recepient_name, :recepient_phone_number, presence: true
   validates :weight, :distance, :price, numericality: { greater_than_or_equal_to: 0 }
 end
-end
