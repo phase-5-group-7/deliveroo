701df1d5139019cf9be34019b01dd9c63279f3ba
benja7 <benjaminmutava@gmail.com>
2023-04-25 11:40:55 +0300

    styled the search button

--- frontend/src/components/searchbar/SearchBar.js
+++ frontend/src/components/searchbar/SearchBar.js
@@ -1,20 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FaSearch } from 'react-icons/fa';
+import './SearchBar.css';
+
+
 
 function SearchBar() {
-  const handleSearch = (event) => {
+  const [searchResults, setSearchResults] = useState([]);
+
+  const handleSearch = async (event) => {
     event.preventDefault();
-    // TODO: Implement search logic
+    const query = event.target.elements.query.value;
+
+    try {
+      const response = await fetch("https://deliveroo-api.onrender.com/order?q=${query}");
+      const data = await response.json();
+      setSearchResults(data);
+    } catch (error) {
+      console.error(error);
+    }
   };
 
   return (
     <div>
       <form onSubmit={handleSearch}>
-        <input type="text" placeholder="Search..." />
+        <input type="text" placeholder="Search..." name="query" />
         <button type="submit">
           <FaSearch />
         </button>
       </form>
+      <ul>
+        {searchResults.map((result) => (
+          <li key={result.id}>{result.title}</li>
+        ))}
+      </ul>
     </div>
   );
 }
