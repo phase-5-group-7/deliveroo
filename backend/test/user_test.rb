9f3aa31ec1356c5f8ab5cbb31164273ca2df38bd
Student <venus.kabera@student.moringaschool.com>
2023-04-28 15:11:24 +0300

    added the second test email

--- backend/test/user_test.rb
+++ backend/test/user_test.rb
@@ -7,8 +7,13 @@ class UserTest < ActiveSupport::TestCase
       email: "",
     )
   end
-  
+
   test "username should be present" do
     @user.username = ""
+    assert_not @user.valid?
+  end
+  
+  test "email should be present" do
+    @user.email = ""
     assert_not @user.valid?
   end
