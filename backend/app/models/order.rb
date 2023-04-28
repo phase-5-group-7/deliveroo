b7a507d8b4baa209d80922fd919c364a5295d4df
LincKim <lincolnkimutai@gmail.com>
2023-04-28 12:48:05 +0300

    Fixed bugs on price calculation

--- backend/app/models/order.rb
+++ backend/app/models/order.rb
@@ -19,6 +19,12 @@ class Order < ApplicationRecord
         answer_dist = (distance - 2)
         weight_dist_price = min_price_weight + min_price_distance
         price = weight_dist_price + (answer_weight * 30).abs + (answer_dist * 70).abs
+      elsif weight <= 5 && distance > 2
+        answer_dist = (distance - 2)
+        price = min_price_weight + (min_price_distance + (answer_dist * 70).abs)
+      elsif weight > 5 && distance <= 2
+        answer_weight = (weight - 5)
+        price = (min_price_weight + (answer_weight * 30).abs) + min_price_distance
       end
     end
 
