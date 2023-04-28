f6b42e5311990ac9b071d250b6c97e4bc3690d50
Student <venus.kabera@student.moringaschool.com>
2023-04-28 15:15:34 +0300

    added first test for order on phne number

---
+++ backend/test/orders_test.rb
@@ -0,0 +1,20 @@
+require 'test_helper'
+
+class CreateOrdersTest < ActiveSupport::TestCase
+  def setup
+    @order = Order.new(
+      name: "",
+      phone_number: "",
+      recepient_name: "",
+      recepient_phone_no: "",
+      description: "",
+      weight: 5.5,
+      pick_up: 1,
+      delivery_drop_off: 2,
+     
+    )
+  end
+  test "phone number should be present" do
+    @order.phone_number = ""
+    assert_not @order.valid?
+  end
