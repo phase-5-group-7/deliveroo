9297b601b519030b7089f756fd17de6488ef3db0
Darylcosm0 <mutaidaryl@gmail.com>
2023-05-01 11:41:23 +0300

    placed a logo and updated navbar

--- frontend/src/components/navbar/Navbar.js
+++ frontend/src/components/navbar/Navbar.js
@@ -2,55 +2,73 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import './Navbar.css';
 import 'bootstrap/dist/css/bootstrap.css';
+import { TransitionGroup, CSSTransition } from 'react-transition-group';
 
-function Navbar({isAuthenticated, setIsAuthenticated}) {
-
+function Navbar({ isAuthenticated, setIsAuthenticated }) {
   const handleLogout = () => {
     // Clear the token from local storage
-    localStorage.removeItem("token");
-    setIsAuthenticated(false)
+    localStorage.removeItem('token');
+    setIsAuthenticated(false);
   };
 
-
   return (
-    <nav className="navbar " >
-       <div>
-        <span id="logo">DELIVEROO</span>
+    <nav className="navbar ">
+      <div>
+        <img src="https://image.similarpng.com/very-thumbnail/2020/06/Fast-delivery-logo-design-vector-PNG.png" alt="Deliveroo Logo" height="40" />
       </div>
       <ul>
-      {isAuthenticated ? ( 
-        <>
-          <li>
-            <Link to="/">HOME</Link>
-          </li>
-          <li >
-            <Link to="/about">ABOUT</Link>
-          </li>
-          <li >
-            <Link id="login" to="/login">LOGIN</Link>
-          </li>
-        </>
-      ) : (
-        <>
-        <li>
-            <Link to="/">HOME</Link>
-          </li>
-          <li >
-            <Link to="/about">ABOUT</Link>
-          </li>
-          <li >
-              <Link to="/orders">ORDER</Link>
-          </li>
-          <li >
-            <Link to="/ordercard">ORDER CARD</Link>
-          </li>
-          
-          <li >
-            <Link onClick={handleLogout} id="login" to="/login">LOGOUT</Link>
-          </li>
-        </>
-        )
-        }
+        {isAuthenticated ? (
+          <>
+            <CSSTransition classNames="fade" timeout={300}>
+              <li>
+                <Link to="/">HOME</Link>
+              </li>
+            </CSSTransition>
+            <CSSTransition classNames="fade" timeout={300}>
+              <li>
+                <Link to="/about">ABOUT</Link>
+              </li>
+            </CSSTransition>
+            <CSSTransition classNames="fade" timeout={300}>
+              <li>
+                <Link id="login" to="/login">
+                  LOGIN
+                </Link>
+              </li>
+            </CSSTransition>
+          </>
+        ) : (
+          <>
+            <CSSTransition classNames="fade" timeout={300}>
+              <li>
+                <Link to="/">HOME</Link>
+              </li>
+            </CSSTransition>
+            <CSSTransition classNames="fade" timeout={300}>
+              <li>
+                <Link to="/about">ABOUT</Link>
+              </li>
+            </CSSTransition>
+            <CSSTransition classNames="fade" timeout={300}>
+              <li>
+                <Link to="/orders">ORDER</Link>
+              </li>
+            </CSSTransition>
+            <CSSTransition classNames="fade" timeout={300}>
+              <li>
+                <Link to="/ordercard">ORDER CARD</Link>
+              </li>
+            </CSSTransition>
+
+            <CSSTransition classNames="fade" timeout={300}>
+              <li>
+                <Link onClick={handleLogout} id="login" to="/login">
+                  LOGOUT
+                </Link>
+              </li>
+            </CSSTransition>
+          </>
+        )}
       </ul>
     </nav>
   );
